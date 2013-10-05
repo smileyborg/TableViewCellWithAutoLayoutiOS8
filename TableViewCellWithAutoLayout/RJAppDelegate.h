c14d32d51452d51c77b9a1484b01d626f8bbb8ad
caoimghgin <caoimghgin@gmail.com>
2013-10-05 14:03:54 -0400

    Initial Commit

---
+++ TableViewCellWithAutoLayout/RJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RJAppDelegate.h
+//  TableViewCellWithAutoLayout
+//
+//  Created by Kevin Muldoon on 10/5/13.
+//  Copyright (c) 2013 RobotJackalope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewCellWithAutoLayoutTests/TableViewCellWithAutoLayoutTests.m
@@ -0,0 +1,34 @@
+//
+//  TableViewCellWithAutoLayoutTests.m
+//  TableViewCellWithAutoLayoutTests
+//
+//  Created by Kevin Muldoon on 10/5/13.
+//  Copyright (c) 2013 RobotJackalope. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewCellWithAutoLayoutTests : XCTestCase
+
+@end
+
+@implementation TableViewCellWithAutoLayoutTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
