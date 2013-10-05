3bf4da7ae925182105bf8e01c001af66f005772a
Tyler Fox <Tyler_Fox@intuit.com>
2013-10-05 15:50:00 -0700

    Fix up dynamic cell heights using Auto Layout

--- TableViewCellWithAutoLayout/TableViewController/RJTableViewCell.m
+++ TableViewCellWithAutoLayout/TableViewController/RJTableViewCell.m
@@ -8,6 +8,13 @@
 
 #import "RJTableViewCell.h"
 
+@interface RJTableViewCell ()
+
+// We'll set this to YES once this instance of the cell has setup its constraints, so that any additional calls to updateConstraints are no-ops
+@property (nonatomic, assign) BOOL hasSetupConstraints;
+
+@end
+
 @implementation RJTableViewCell
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
@@ -15,7 +22,7 @@
     if (self) {
         // Initialization code
         
-        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectNull];
+        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
         [self.titleLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
         [self.titleLabel setLineBreakMode:NSLineBreakByTruncatingTail];
         [self.titleLabel setAdjustsFontSizeToFitWidth:YES];
@@ -25,19 +32,33 @@
         [self.titleLabel setTextColor:[UIColor blackColor]];
         [self.titleLabel setBackgroundColor:[UIColor lightGrayColor]];
         
-        self.bodyLabel = [[UILabel alloc] initWithFrame:CGRectNull];
+        self.bodyLabel = [[UILabel alloc] initWithFrame:CGRectZero];
         [self.bodyLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
         [self.bodyLabel setLineBreakMode:NSLineBreakByTruncatingTail];
-        [self.bodyLabel setAdjustsFontSizeToFitWidth:YES];
+        // You don't want the below line; let the font size for the multi-line body label remain fixed.
+//        [self.titleLabel setAdjustsFontSizeToFitWidth:YES];
         [self.bodyLabel setNumberOfLines:0];
         [self.bodyLabel setTextAlignment:NSTextAlignmentLeft];
         [self.bodyLabel setFont: [UIFont fontWithName:@"Helvetica" size:11]];
         [self.bodyLabel setTextColor:[UIColor blackColor]];
         [self.bodyLabel setBackgroundColor:[UIColor yellowColor]];
+        
+        // If you don't do this, the auto layout solver engine may calculate a ever-so-slightly smaller height than actually required
+        // (probably due to rounding errors internally) which will cause the bodyLabel to truncate the last line. This will force
+        // the bodyLabel to always get the size it needs for all the lines. (The titleLabel height will be reduced ever so slightly
+        // in this case if needed to satisfy this constraint, but in this case it shouldn't be noticeable.)
+        // Another way to solve this is to return a slightly larger cell height (add 1 point) than the auto layout calculations return.
+        // Or a third way is to build in a few points of padding (perhaps between the labels, or above or below them) which has a priority of
+        // UILayoutPriorityDefaultHigh - 1, so that it will be the first thing to be reduced if the cell height is a tiny bit too small -
+        // it will act as a buffer to prevent the labels from getting smaller than the size they really want.
+        [self.bodyLabel setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
 
+        // Add your subviews right away during init - no reason to wait until later
+        [self.contentView addSubview:self.titleLabel];
+        [self.contentView addSubview:self.bodyLabel];
     }
     
-    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+//    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
     
     return self;
 }
@@ -45,24 +66,22 @@
 - (void)willMoveToSuperview:(UIView *)newSuperview {
     [super willMoveToSuperview:newSuperview];
     
-    [self.contentView addSubview:self.titleLabel];
-    [self.contentView addSubview:self.bodyLabel];
-    
-    [self layoutSubviews];
-    [self updateConstraints];
-    
-    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+//    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
     
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
-    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+//    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
 }
 
 - (void)updateConstraints {
     [super updateConstraints];
     
+    if (self.hasSetupConstraints) {
+        return;
+    }
+    
     [self.contentView addConstraint:[NSLayoutConstraint
                                      constraintWithItem:self.titleLabel
                                      attribute:NSLayoutAttributeLeading
@@ -70,7 +89,7 @@
                                      toItem:self.contentView
                                      attribute:NSLayoutAttributeLeading
                                      multiplier:1.0f
-                                     constant:20.0f]];
+                                     constant:kLabelHorizontalInsets]];
     
     [self.contentView addConstraint:[NSLayoutConstraint
                                      constraintWithItem:self.titleLabel
@@ -88,16 +107,17 @@
                                      toItem:self.contentView
                                      attribute:NSLayoutAttributeTrailing
                                      multiplier:1.0f
-                                     constant:-20.0f]];
+                                     constant:-kLabelHorizontalInsets]];
     
-    [self.contentView  addConstraint:[NSLayoutConstraint
-                                      constraintWithItem:self.titleLabel
-                                      attribute:NSLayoutAttributeHeight
-                                      relatedBy:NSLayoutRelationLessThanOrEqual
-                                      toItem:nil
-                                      attribute:NSLayoutAttributeNotAnAttribute
-                                      multiplier:1.0f
-                                      constant:22.0f]];
+    // You don't need the below constraint - the auto-generated content compression resistance constraints on UILabel are sufficient!
+//    [self.contentView  addConstraint:[NSLayoutConstraint
+//                                      constraintWithItem:self.titleLabel
+//                                      attribute:NSLayoutAttributeHeight
+//                                      relatedBy:NSLayoutRelationGreaterThanOrEqual
+//                                      toItem:nil
+//                                      attribute:NSLayoutAttributeNotAnAttribute
+//                                      multiplier:1.0f
+//                                      constant:22.0f]];
     
     ///////////////////////////////////////////////////////////////////////////////////////////
     
@@ -108,7 +128,7 @@
                                       toItem:self.contentView
                                       attribute:NSLayoutAttributeLeading
                                       multiplier:1.0f
-                                      constant:20.0f]];
+                                      constant:kLabelHorizontalInsets]];
     
     [self.contentView  addConstraint:[NSLayoutConstraint
                                       constraintWithItem:self.bodyLabel
@@ -126,7 +146,7 @@
                                       toItem:self.contentView
                                       attribute:NSLayoutAttributeTrailing
                                       multiplier:1.0f
-                                      constant:-20.0f]];
+                                      constant:-kLabelHorizontalInsets]];
     
     [self.contentView  addConstraint:[NSLayoutConstraint
                                       constraintWithItem:self.bodyLabel
@@ -137,16 +157,19 @@
                                       multiplier:1.0f
                                       constant:-20.0f]];
     
-    [self.contentView  addConstraint:[NSLayoutConstraint
-                                      constraintWithItem:self.bodyLabel
-                                      attribute:NSLayoutAttributeHeight
-                                      relatedBy:NSLayoutRelationGreaterThanOrEqual
-                                      toItem:nil
-                                      attribute:NSLayoutAttributeNotAnAttribute
-                                      multiplier:1.0f
-                                      constant:22.0f]];
+    // You don't need the below constraint - the auto-generated content compression resistance constraints on UILabel are sufficient!
+//    [self.contentView  addConstraint:[NSLayoutConstraint
+//                                      constraintWithItem:self.bodyLabel
+//                                      attribute:NSLayoutAttributeHeight
+//                                      relatedBy:NSLayoutRelationGreaterThanOrEqual
+//                                      toItem:nil
+//                                      attribute:NSLayoutAttributeNotAnAttribute
+//                                      multiplier:1.0f
+//                                      constant:22.0f]];
     
-    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+    self.hasSetupConstraints = YES;
+    
+//    NSLog(@"%@ %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
     
 }
 
