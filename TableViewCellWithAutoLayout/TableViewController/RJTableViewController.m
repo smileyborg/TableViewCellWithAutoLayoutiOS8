8401a88444cc7cc924670123c9c6eb5c831711a7
Tyler Fox <Tyler_Fox@intuit.com>
2014-02-03 08:44:54 -0800

    Add comments to clarify setting the cell's bounds for atypical cases

--- TableViewCellWithAutoLayout/TableViewController/RJTableViewController.m
+++ TableViewCellWithAutoLayout/TableViewController/RJTableViewController.m
@@ -160,11 +160,15 @@ static NSString *CellIdentifier = @"CellIdentifier";
     [cell setNeedsUpdateConstraints];
     [cell updateConstraintsIfNeeded];
     
-    // Set the width of the cell to match the width of the table view. This is important so that we'll get the
-    // correct height for different table view widths, since our cell's height depends on its width due to
-    // the multi-line UILabel word wrapping. Don't need to do this above in -[tableView:cellForRowAtIndexPath]
-    // because it happens automatically when the cell is used in the table view.
+    // The cell's width must be set to the same size it will end up at once it is in the table view.
+    // This is important so that we'll get the correct height for different table view widths, since our cell's
+    // height depends on its width due to the multi-line UILabel word wrapping. Don't need to do this above in
+    // -[tableView:cellForRowAtIndexPath:] because it happens automatically when the cell is used in the table view.
     cell.bounds = CGRectMake(0.0f, 0.0f, CGRectGetWidth(tableView.bounds), CGRectGetHeight(cell.bounds));
+    // NOTE: if you are displaying a section index (e.g. alphabet along the right side of the table view), or
+    // if you are using a grouped table view style where cells have insets to the edges of the table view,
+    // you'll need to adjust the cell.bounds.size.width to be smaller than the full width of the table view we just
+    // set it to above. See http://stackoverflow.com/questions/3647242 for discussion on the section index width.
     
     // Do the layout pass on the cell, which will calculate the frames for all the views based on the constraints
     // (Note that the preferredMaxLayoutWidth is set on multi-line UILabels inside the -[layoutSubviews] method
