1789247bea400b53763bb3f244d7530a81989d5a
Morten Bøgh <mob@trifork.com>
2014-06-09 16:26:25 +0200

    Added a nib based TableViewCell

---
+++ TableViewCellWithAutoLayout/TableViewController/NibTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NibTableViewCell.swift
+//  TableViewCellWithAutoLayout
+//
+//  Created by Morten Bøgh on 09/06/14.
+//  Copyright (c) 2014 RobotJackalope. All rights reserved.
+//
+
+import UIKit
+
+class NibTableViewCell: UITableViewCell {
+    
+}
