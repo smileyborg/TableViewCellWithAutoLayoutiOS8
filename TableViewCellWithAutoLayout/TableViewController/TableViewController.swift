37124458cfa47f58d503d9ce6d1475b09f0a36bc
Morten Bøgh <mob@trifork.com>
2014-06-09 21:15:24 +0200

    Adjusted the styling

--- TableViewCellWithAutoLayout/TableViewController/TableViewController.swift
+++ TableViewCellWithAutoLayout/TableViewController/TableViewController.swift
@@ -37,7 +37,7 @@ class TableViewController : UITableViewController
         
         // Select either a programatically created cell or a nib-based one.
         tableView.registerClass(TableViewCell.self, forCellReuseIdentifier: kCellIdentifier)
-//        tableView.registerNib(UINib(nibName: "NibTableViewCell", bundle: NSBundle.mainBundle()), forCellReuseIdentifier: kCellIdentifier)
+        // tableView.registerNib(UINib(nibName: "NibTableViewCell", bundle: NSBundle.mainBundle()), forCellReuseIdentifier: kCellIdentifier)
         
         // Self-sizing table view cells in iOS 8 require that the rowHeight property of the table view be set to the constant UITableViewAutomaticDimension
         tableView.rowHeight = UITableViewAutomaticDimension
