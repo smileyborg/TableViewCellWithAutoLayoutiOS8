19a58be49ab9a020cebfc2bb8e679eab810f18f7
Tyler Fox <Tyler_Fox@intuit.com>
2014-09-22 08:48:31 -0700

    Call [super updateConstraints] as the final step in updateConstraints

--- TableViewCellWithAutoLayout/TableViewController/TableViewCell.swift
+++ TableViewCellWithAutoLayout/TableViewController/TableViewCell.swift
@@ -57,35 +57,35 @@ class TableViewCell: UITableViewCell
     
     override func updateConstraints()
     {
-        super.updateConstraints()
-        
-        if didSetupConstraints { return }
-        
-        // Note: if the constraints you add below require a larger cell size than the current size (which is likely to be the default size {320, 44}), you'll get an exception.
-        // As a fix, you can temporarily increase the size of the cell's contentView so that this does not occur using code similar to the line below.
-        //      See here for further discussion: https://github.com/Alex311/TableCellWithAutoLayout/commit/bde387b27e33605eeac3465475d2f2ff9775f163#commitcomment-4633188
-        // contentView.bounds = CGRect(x: 0.0, y: 0.0, width: 99999.0, height: 99999.0)
-        
-        // Prevent the two UILabels from being compressed below their intrinsic content height
-        // FIXME 7-Jun-14 Xcode 6b1: Apple Bug Report rdar://17220525: The UILayoutPriority enum is not compatible with Swift yet!
-        // As a temporary workaround, we're using the raw value of UILayoutPriorityRequired = 1000
-        UIView.autoSetPriority(1000) {
-            self.titleLabel.autoSetContentCompressionResistancePriorityForAxis(.Vertical)
-            self.bodyLabel.autoSetContentCompressionResistancePriorityForAxis(.Vertical)
+        if !didSetupConstraints {
+            // Note: if the constraints you add below require a larger cell size than the current size (which is likely to be the default size {320, 44}), you'll get an exception.
+            // As a fix, you can temporarily increase the size of the cell's contentView so that this does not occur using code similar to the line below.
+            //      See here for further discussion: https://github.com/Alex311/TableCellWithAutoLayout/commit/bde387b27e33605eeac3465475d2f2ff9775f163#commitcomment-4633188
+            // contentView.bounds = CGRect(x: 0.0, y: 0.0, width: 99999.0, height: 99999.0)
+            
+            // Prevent the two UILabels from being compressed below their intrinsic content height
+            // FIXME 7-Jun-14 Xcode 6b1: Apple Bug Report rdar://17220525: The UILayoutPriority enum is not compatible with Swift yet!
+            // As a temporary workaround, we're using the raw value of UILayoutPriorityRequired = 1000
+            UIView.autoSetPriority(1000) {
+                self.titleLabel.autoSetContentCompressionResistancePriorityForAxis(.Vertical)
+                self.bodyLabel.autoSetContentCompressionResistancePriorityForAxis(.Vertical)
+            }
+            
+            titleLabel.autoPinEdgeToSuperviewEdge(.Top, withInset: kLabelVerticalInsets)
+            titleLabel.autoPinEdgeToSuperviewEdge(.Leading, withInset: kLabelHorizontalInsets)
+            titleLabel.autoPinEdgeToSuperviewEdge(.Trailing, withInset: kLabelHorizontalInsets)
+            
+            // This constraint is an inequality so that if the cell is slightly taller than actually required, extra space will go here
+            bodyLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: titleLabel, withOffset: 10.0, relation: .GreaterThanOrEqual)
+            
+            bodyLabel.autoPinEdgeToSuperviewEdge(.Leading, withInset: kLabelHorizontalInsets)
+            bodyLabel.autoPinEdgeToSuperviewEdge(.Trailing, withInset: kLabelHorizontalInsets)
+            bodyLabel.autoPinEdgeToSuperviewEdge(.Bottom, withInset: kLabelVerticalInsets)
+            
+            didSetupConstraints = true
         }
-        
-        titleLabel.autoPinEdgeToSuperviewEdge(.Top, withInset: kLabelVerticalInsets)
-        titleLabel.autoPinEdgeToSuperviewEdge(.Leading, withInset: kLabelHorizontalInsets)
-        titleLabel.autoPinEdgeToSuperviewEdge(.Trailing, withInset: kLabelHorizontalInsets)
-        
-        // This constraint is an inequality so that if the cell is slightly taller than actually required, extra space will go here
-        bodyLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: titleLabel, withOffset: 10.0, relation: .GreaterThanOrEqual)
-        
-        bodyLabel.autoPinEdgeToSuperviewEdge(.Leading, withInset: kLabelHorizontalInsets)
-        bodyLabel.autoPinEdgeToSuperviewEdge(.Trailing, withInset: kLabelHorizontalInsets)
-        bodyLabel.autoPinEdgeToSuperviewEdge(.Bottom, withInset: kLabelVerticalInsets)
-        
-        didSetupConstraints = true
+    
+        super.updateConstraints()
     }
     
     func updateFonts()
